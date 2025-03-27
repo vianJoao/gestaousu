c352cb03b626df9e0b4770c8428b26a8bd35b54c
João (Jovi) <158322472+vianJoao@users.noreply.github.com>
2025-03-27 11:50:54 -0300

    JIRA-01--27-03-25-0.0.1

---
+++ back-end/gestaousuarios/src/main/java/com/usuarios/gestaousuarios/GestaousuariosApplication.java
@@ -0,0 +1,13 @@
+package com.usuarios.gestaousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaousuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaousuariosApplication.class, args);
+	}
+
+}
