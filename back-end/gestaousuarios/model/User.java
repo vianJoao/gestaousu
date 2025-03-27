ee3f798c68f4f07c1b182228a0f5c8fc43bb6eb9
João (Jovi) <158322472+vianJoao@users.noreply.github.com>
2025-03-27 12:53:51 -0300

    JIRA-01--27-03-25-0.0.2

---
+++ back-end/gestaousuarios/model/User.java
@@ -0,0 +1,34 @@
+package com.usuarios.gestaousuarios.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+}
